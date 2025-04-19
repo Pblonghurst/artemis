a9923cd674aa1032499bd5efa7c926d7c588e635
Pblonghurst <59968417+Phil-Longhurst@users.noreply.github.com>
2025-04-19 16:42:06 +0100

    login logic fixed

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,18 @@ export default function HomePage() {
   return (
     <main className="flex items-center justify-center h-screen">
       <a
-        href="/auth/login"
+        href="auth/login"
         className="px-4 py-2 bg-black text-white rounded"
       >
         Login to Artemis
+
+      </a>
+
+        <a
+        href="auth/logout"
+        className="px-4 py-2 bg-black text-white rounded"
+      >
+        Logout of Artemis
       </a>
     </main>
   );
